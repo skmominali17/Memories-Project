80bdb789345579d4b2e226e239902704ce5acbc7
Momin <knightrideraiz@gmail.com>
2023-08-24 17:27:37 +0530

    changes made in pkg.json, .gitignore and in index.js

--- server/index.js
+++ server/index.js
@@ -3,7 +3,8 @@ import express from "express";
 import bodyParser from "body-parser"
 import mongoose from "mongoose";
 import cors from "cors";
-import postRoutes from "./routes/posts.js"
+import postRoutes from "./routes/posts.js";
+import dotenv from "dotenv"
 
 //creating app
 const app = express();
@@ -15,8 +16,10 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-// Building Connections
-const CONNECTION_URL = "mongodb+srv://mominalipubg:dash123@memoriescluster.hk8uz4a.mongodb.net/?retryWrites=true&w=majority";
-const port = process.env.PORT || 5000;
+dotenv.config();
 
-mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true}).then(()=> app.listen(port, ()=> console.log(`Server running on port ${port}`))).catch((err) => console.log(err.message));
+// Building Connections
+const PORT = process.env.PORT || 5000;
+const CONNECTION_URL = process.env.CONNECTION_URL;
+
+mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true}).then(()=> app.listen(PORT, ()=> console.log(`Server running on port ${PORT}`))).catch((err) => console.log(err.message));
