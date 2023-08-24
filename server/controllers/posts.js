cdfa0afcd80a470155698578d8123721240e517b
Momin <knightrideraiz@gmail.com>
2023-08-24 18:31:11 +0530

    again pkg.json

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -8,7 +8,6 @@ const router = express.Router();
 export const getPosts = async (req, res) => { 
     try {
         const postMessages = await PostMessage.find();
-                
         res.status(200).json(postMessages);
     } catch (error) {
         res.status(404).json({ message: error.message });
